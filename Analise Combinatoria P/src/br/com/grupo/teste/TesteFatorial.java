9210bc36dd4ddbcbe7081b11b032124b295bd2aa
GugaE <gustavo_enndi@hotmail.com>
2016-06-01 17:02:12 -0300

    segunda questão

--- Analise Combinatoria P/src/br/com/grupo/teste/TesteFatorial.java
+++ Analise Combinatoria P/src/br/com/grupo/teste/TesteFatorial.java
@@ -7,6 +7,7 @@ public class TesteFatorial {
 
 	public static void main(String[] args) {
 		Combinatoria comb = new Combinatoria();
+		
 		List<Integer> lista = Arrays.asList(1,2,3,4,5);
 		List<Character> arr = Arrays.asList('A','M','O','R','A');
 		List<String> arr2 = Arrays.asList("A","B","C","D");
