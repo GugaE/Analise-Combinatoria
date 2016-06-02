17a1311945d202a22200a4cbfc6db336b779d172
GugaE <gustavo_enndi@hotmail.com>
2016-06-02 16:58:16 -0300

    finalizado a segunda questao da ficha com todas as letras

--- Analise Combinatoria P/src/br/com/grupo/modelo/Combinatoria.java
+++ Analise Combinatoria P/src/br/com/grupo/modelo/Combinatoria.java
@@ -1,33 +1,77 @@
 package br.com.grupo.modelo;
+
 import java.util.*;
 
 public class Combinatoria {
-	
-	public void printCombinacoes(List<Integer> list, int r){
-		
+
+	public void printCombinacoes(List<Integer> list, int r) {
+
 	}
-	public int combination(List<?> lista, int r){
-		int nFatorial = lista.size();	
+
+	public int combinationSimples(List<?> lista, int r) {
+		int nFatorial = lista.size();
 		return fatorial(nFatorial) / (fatorial(r) * (nFatorial - r));
 	}
-	public int arrangement(List<?> lista, int r){
+
+	public int arrangementSimples(List<?> lista, int r) {
 		int nFatorial = lista.size();
-		return fatorial(nFatorial) /(nFatorial - r);
+		return fatorial(nFatorial) / (nFatorial - r);
 	}
-	public int permutation(List<?> lista){
+
+	public int permutationSimples(List<?> lista) {
 		return fatorial(lista.size());
 	}
-	private int fatorial(int fat){
+	
+	public int permutationRepeticao(List<String> lista){
+		List<Integer> x = elementsRepetidos(lista);
+		int[] vetRepetidos = new int[x.size()];
 		
-		if(fat > 1 ){
-			return  fat * fatorial(fat - 1);
-			
+		int valorDaMutElementosRepetidos= 1;
+		int nfat =  fatorial(lista.size());
+		
+		for (int i = 0; i < vetRepetidos.length; i++) {
+			vetRepetidos[i] = x.get(i);
+			vetRepetidos[i] = fatorial(vetRepetidos[i]);
+			valorDaMutElementosRepetidos = vetRepetidos[i] * valorDaMutElementosRepetidos;
 		}
-		else if(fat == 1 || fat == 0){
+		return nfat / valorDaMutElementosRepetidos;
+	}
+	
+	private int fatorial(int fat) {
+
+		if (fat > 1) {
+			return fat * fatorial(fat - 1);
+		} else if (fat == 1 || fat == 0) {
 			return 1;
 		}
-		
-		
 		return 0;
+	}
+
+	private List<Integer> elementsRepetidos(List<String> lista) {
+		List<Integer> listaNumeros = new ArrayList<>();
+		int contador = 0;
+		boolean saida = true;
+		Collections.sort(lista);
+		do {
+			int intervaloAtual = lista.lastIndexOf(lista.get(contador)) - lista.indexOf(lista.get(contador)) + 1;
+			if(intervaloAtual > 1){
+				listaNumeros.add(intervaloAtual);
+				contador = lista.lastIndexOf(lista.get(contador)) + 1;
+			}
+			if(intervaloAtual <= 1 ){
+				contador += 1;
+			}
+			if(contador >= lista.size()){
+				saida = false;
+			}
+			
+		} while (saida);
+			
+			
+			
+		
+				
+		return listaNumeros;
+
 	}
 }
--- Analise Combinatoria P/src/br/com/grupo/teste/TesteFatorial.java
+++ Analise Combinatoria P/src/br/com/grupo/teste/TesteFatorial.java
@@ -8,12 +8,11 @@ public class TesteFatorial {
 	public static void main(String[] args) {
 		Combinatoria comb = new Combinatoria();
 		
-		List<Integer> lista = Arrays.asList(1,2,3,4,5);
-		List<Character> arr = Arrays.asList('A','M','O','R','A');
-		List<String> arr2 = Arrays.asList("A","B","C","D");
+		//List<Integer> lista = Arrays.asList(1,2,3,4,5);
+		//List<Character> arr = Arrays.asList('A','M','O','R','A');
+		List<String> arr2 = Arrays.asList("A","B","C","A");
 		
-		System.out.println(comb.permutation(lista));
-
+		System.out.println(comb.permutationRepeticao(arr2));
 	}
 
 }
